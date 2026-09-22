ffea6ab979b94152ce198c182c5d7c555406225b
Antigorila <antigorilla.666@gmail.com>
2026-09-22 14:32:53 +0200

    ini

---
+++ Week_2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Week_2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Week_2/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_2
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsUrgent { get; set; }
+    }
+}
